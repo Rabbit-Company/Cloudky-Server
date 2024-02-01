ed88ccc556f58bdf2ecc82eeed1087518bdda9fa
Ziga Zajc <ziga.zajc007@gmail.com>
2024-02-01 09:48:50 +0100

    Update

--- server/endpoints/v1/file/upload.ts
+++ server/endpoints/v1/file/upload.ts
@@ -8,6 +8,11 @@ import Storage from "../../../storage/storage";
 export default async function handleFileUpload(req: Request, match: MatchedRoute | null, ip: string | undefined): Promise<Response> {
 	if(req.method !== 'POST') return Utils.jsonResponse(Errors.getJson(404));
 
+	const contentLength = req.headers.get('Content-Length');
+	if (contentLength && parseInt(contentLength, 10) > 53_687_091_200) {
+    return Utils.jsonResponse(Errors.getJson(1010));
+  }
+
 	const auth = Utils.basicAuthentication(req);
 	if(auth === null) return Utils.jsonResponse(Errors.getJson(1018));
 
@@ -23,7 +28,18 @@ export default async function handleFileUpload(req: Request, match: MatchedRoute
 	const key = formdata.get('name')?.toString();
 	const file = formdata.get('file');
 
-	if(!Validate.userFilePathName(key)) return Utils.jsonResponse(Errors.getJson(2000));
+	if(!Validate.userFilePathName(key)) return Utils.jsonResponse(Errors.getJson(1005));
+	if (!(file instanceof File) || file.size === undefined) {
+    return Utils.jsonResponse(Errors.getJson(1006));
+  }
+
+	const fileSizeInBytes = file.size || 0;
+	const fileSizeInMB = fileSizeInBytes / (1024 * 1024);
+
+	// 50GB max file size
+	if(fileSizeInMB > 51_200){
+		return Utils.jsonResponse(Errors.getJson(1010));
+	}
 
 	let res = await Storage.uploadUserFile(auth.user, key!, file);
 	if(res === null) return Utils.jsonResponse(Errors.getJson(2000));
