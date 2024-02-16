016f766b73123794de5263865b2ddeb5e2df6339
Ziga Zajc <ziga.zajc007@gmail.com>
2024-02-16 13:36:43 +0100

    Protected metrics API endpoint with bearer token

--- server/endpoints/metrics.ts
+++ server/endpoints/metrics.ts
@@ -6,5 +6,12 @@ import Redis from "../database/redis";
 export default async function handleMetrics(req: Request, match: MatchedRoute | null, ip: string | undefined): Promise<Response> {
 	if(req.method !== 'GET' || Number(process.env.METRICS_TYPE) < 1) return Utils.jsonResponse(Errors.getJson(404));
 
+	const token = process.env.METRICS_TOKEN;
+	if(token !== 'none'){
+		const bearer = Utils.getBearerToken(req);
+		if(bearer === null) return Utils.jsonResponse(Errors.getJson(1000));
+		if(bearer !== token) return Utils.jsonResponse(Errors.getJson(1008));
+	}
+
 	return new Response(await Redis.getString(`metrics_cache`));
 }
